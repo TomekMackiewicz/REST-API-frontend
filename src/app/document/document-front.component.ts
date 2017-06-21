724605dd1317a997bf8e85d85849f06f0cbb3ed0
root <tomasz.mackiewicz79@gmail.com>
2017-06-21 13:19:01 +0200

    Transaction Method

--- src/app/document/document-front.component.ts
+++ src/app/document/document-front.component.ts
@@ -29,45 +29,59 @@ export class DocumentFrontComponent implements OnInit {
             .switchMap((params: Params) => this.documentService.getDocument(+params['document']))
             .subscribe(category => this.document = category);
     }
-    
-    processTransaction() {
-        //window.location.href='http://www.cnn.com/';
-//        let headers = new Headers({
-//            'Content-Type': 'application/json',
-//            'Authorization': 'Bearer d9a4536e-62ba-4f60-8017-6053211d3f47'
-//        });
-//        let body = {
-//            continueUrl: 'http://localhost:4200/documents',
-//            currencyCode: 'PLN',
-//            customerIp: '127.0.0.1',
-//            description: 'Document',
-//            merchantPosId: '300746',
-//            notifyUrl: 'http://localhost:8000/transaction/notify',
-//            product: 'Dokument',
-//            totalAmount: '1000',
-//            'OpenPayu-Signature': 'sender=145227;algorithm=SHA-256;signature=bc94a8026d6032b5e216be112a5fb7544e66e23e68d44b4283ff495bdb3983a8'};
-//            
-//        console.log(body, headers);
-//
-//        return this.http.post('https://secure.snd.payu.com/api/v2_1/orders', body, headers);        
-                
-       
-    }
+
+    //    processTransaction() {
+    //        let orderNo = 1;
+    //        let headers = new Headers({
+    //            'Content-Type': 'application/json',
+    //            'Authorization': 'Bearer d9a4536e-62ba-4f60-8017-6053211d3f47'
+    //        });
+    //        let order = {
+    //            continueUrl: 'http://localhost:8000/payu/success/1234',
+    //            currencyCode: 'PLN',
+    //            customerIp: '127.0.0.1',
+    //            description: 'Document',
+    //            merchantPosId: '300746',
+    //            notifyUrl: 'http://localhost:8000/payu/notify',
+    //            product: 'Dokument',
+    //            totalAmount: '1000',
+    //            'OpenPayu-Signature': 'sender=145227;algorithm=SHA-256;signature=bc94a8026d6032b5e216be112a5fb7544e66e23e68d44b4283ff495bdb3983a8'
+    //        };
+    //        this.documentService.processTransaction(order).subscribe(
+    //            data => {
+    //                //this.alertService.success('Document created.');
+    //                return true;
+    //            },
+    //            error => {
+    //                //this.alertService.error("Error saving document! " + error);
+    //                return Observable.throw(error);
+    //            }
+    //        );
+    //    }
+
+    //    processTransaction() {
+    //        //window.location.href='http://www.cnn.com/';
+    //        let orderNo = 1;
+    //        let headers = new Headers({
+    //            'Content-Type': 'application/json',
+    //            'Authorization': 'Bearer d9a4536e-62ba-4f60-8017-6053211d3f47'
+    //        });
+    //        let body = {
+    //            continueUrl: 'http://localhost:8000/payu/success/1234',
+    //            currencyCode: 'PLN',
+    //            customerIp: '127.0.0.1',
+    //            description: 'Document',
+    //            merchantPosId: '300746',
+    //            notifyUrl: 'http://localhost:8000/payu/notify',
+    //            product: 'Dokument',
+    //            totalAmount: '1000',
+    //            'OpenPayu-Signature': 'sender=145227;algorithm=SHA-256;signature=bc94a8026d6032b5e216be112a5fb7544e66e23e68d44b4283ff495bdb3983a8'
+    //        };
+    //        console.log(headers);
+    //        return this.http.post('http://localhost:8000/order/pay' + orderNo, body);
+    //        //return this.http.post('https://secure.snd.payu.com/api/v2_1/orders', body, headers);
+    //
+    //
+    //    }
 
 }
-
-
-//<form ngNoForm id="payu-payment-form" method="post" action="https://secure.snd.payu.com/api/v2_1/orders">
-//                <input type="hidden" name="continueUrl" value="http://localhost:4200/documents/">
-//                <input type="hidden" name="currencyCode" value="PLN">
-//                <input type="hidden" name="customerIp" value="127.0.0.1">
-//                <input type="hidden" name="description" value="Order description">
-//                <input type="hidden" name="merchantPosId" value="300746">
-//                <input type="hidden" name="notifyUrl" value="http://localhost:8000/transaction/notify">
-//                <input type="hidden" name="products[0].name" value="Dokument">
-//                <input type="hidden" name="products[0].quantity" value="1">
-//                <input type="hidden" name="products[0].unitPrice" value="1000">
-//                <input type="hidden" name="totalAmount" value="1000">
-//                <input type="hidden" name="OpenPayu-Signature" value="sender=145227;algorithm=SHA-256;signature=bc94a8026d6032b5e216be112a5fb7544e66e23e68d44b4283ff495bdb3983a8">
-//                <button type="submit" formtarget="_blank"></button>
-//            </form>
