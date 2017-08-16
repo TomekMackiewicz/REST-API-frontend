30d4c10b8d3ef994e3bc4773d4913da198ba662f
root <tomasz.mackiewicz79@gmail.com>
2017-08-16 12:43:29 +0200

    Fix logged in after refresh issue

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,20 @@
-import {Component, ViewEncapsulation, OnDestroy} from '@angular/core';
-import {Subscription} from 'rxjs/Subscription';
-import {AuthenticationService} from './services/authentication.service';
-import {TranslateService} from '@ngx-translate/core';
-import {Location} from '@angular/common';
-import {Router, ActivatedRoute} from '@angular/router';
-import {trigger, animate, style, group, animateChild, query, stagger, transition} from '@angular/animations';
+import { Component, ViewEncapsulation, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs/Subscription';
+import { AuthenticationService } from './services/authentication.service';
+import { TranslateService } from '@ngx-translate/core';
+import { Location } from '@angular/common';
+import { Router, ActivatedRoute } from '@angular/router';
+import { trigger, animate, style, group, animateChild, query, stagger, transition } from '@angular/animations';
 
 const fadeIn = [
-    query(':leave', style({position: 'absolute', left: 0, right: 0, opacity: 1}), { optional: true }),
-    query(':enter', style({position: 'absolute', left: 0, right: 0, opacity: 0}), { optional: true }),
+    query(':leave', style({ position: 'absolute', left: 0, right: 0, opacity: 1 }), { optional: true }),
+    query(':enter', style({ position: 'absolute', left: 0, right: 0, opacity: 0 }), { optional: true }),
     group([
         query(':leave',
-            animate('.3s', style({opacity: 0})),
+            animate('.3s', style({ opacity: 0 })),
             { optional: true }),
         query(':enter',
-            animate('.3s .3s', style({opacity: 1})),
+            animate('.3s .3s', style({ opacity: 1 })),
             { optional: true })
     ])
 ];
@@ -31,7 +31,7 @@ const fadeIn = [
     ]
 })
 
-export class AppComponent implements OnDestroy {
+export class AppComponent implements OnInit, OnDestroy {
 
     title = 'Main page';
     username: any;
@@ -41,21 +41,19 @@ export class AppComponent implements OnDestroy {
         private translate: TranslateService,
         private authenticationService: AuthenticationService
     ) {
-        this.subscription = this.authenticationService
-            .getUsername()
-            .subscribe(username => {this.username = username;});
-
         translate.addLangs(["pl", "en", "uk"]);
         translate.setDefaultLang('pl');
         let browserLang = translate.getBrowserLang();
         translate.use(browserLang.match(/pl|en/) ? browserLang : 'pl');
+        this.username = localStorage.getItem('currentUsername');
     }
 
-    //ngOnInit() {}
+    ngOnInit() {
+        this.authenticationService.getUsername().subscribe(currentUsername => this.username = currentUsername);
+    }
 
     prepareRouteTransition(outlet) {
         const animation = outlet.activatedRouteData['animation'] || {};
-        //console.log(animation['value']);
         return animation['value'] || null;
     }
 
