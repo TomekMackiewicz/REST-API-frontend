f138b5eea24e43e48df9b4b6e27e434b581d605f
root <tomasz.mackiewicz79@gmail.com>
2017-09-21 16:39:13 +0200

    Add html to pdf

--- src/app/text/text-full.component.ts
+++ src/app/text/text-full.component.ts
@@ -5,6 +5,7 @@ import { saveAs } from 'file-saver';
 import { AlertService } from '../alert/alert.service';
 import { LoaderService } from '../services/loader.service';
 import * as jsPDF from "jspdf";
+import * as html2canvas from "html2canvas";
 
 @Component({
     selector: 'text-full',
@@ -44,18 +45,35 @@ export class TextFullComponent implements OnInit {
     }    
 
     downloadPDF() {
-//        let title = '<h1 style="text-align: center;">' + this.text.title + '</h1>';
-//        let body = this.text.body;
-//        let content = title.concat(body);
-//        let blob = new Blob([content], {type: "application/pdf;charset=utf-8"});
-//        saveAs(blob, this.text.title + ".pdf");          
-        //this.htmlToText();
-        var doc = new jsPDF();
-        doc.text(20, 20, this.text.title);
-        doc.text(20, 30, this.text.body);
+let doc = new jsPDF();
+
+// Add a title to your PDF
+doc.setFontSize(30); 
+doc.text(12, 10, this.text.title);
+
+// Create your table here (The dynamic table needs to be converted to canvas).
+let element = this.text.body;
+html2canvas(element)
+.then((canvas: any) => {
+    doc.addImage(canvas.toDataURL("image/jpeg"), "JPEG", 0, 50, doc.internal.pageSize.width, element.offsetHeight / 5 );
+    doc.save(`test.pdf`);
+})        
+        //let text = this.text.body;
+//const elementToPrint = this.text.body; //The html element to become a pdf
+//const pdf = new jsPDF('p', 'pt', 'a4');
+//pdf.addHTML(elementToPrint, () => {
+//    pdf.save('web.pdf');
+//});        
+        //let ttt = html2canvas(text); 
+             
+        //let body = html2canvas(this.text.body);
+        //let doc = new jsPDF();  
+        //doc.text(10, 10, text);      
+        //doc.text(20, 20, this.text.title);
+        //doc.text(20, 30, this.text.body);
         //doc.addPage();
         //doc.text(20, 20, 'Do you like that?');
-        doc.save(this.text.title + ".pdf");
+        //doc.save(this.text.title + ".pdf");
     }
 
     downloadDOC() {
